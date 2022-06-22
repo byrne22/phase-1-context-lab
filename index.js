ea2dc6f9c5ccba04e47e18c03f15170522fc32c6
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-22 00:04:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+
 /* Your Code Here */
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
  sneaky bug that we'll cover in the next few lessons!
-
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+ const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +20,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
