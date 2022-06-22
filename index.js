b84cec5f85acac8eb45aba68baca8989802ac493
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-22 00:16:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let createEmployeeRecord = function(row){
+/* Your Code Here */
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */let createEmployeeRecord = function(row){
     return {
         firstName: row[0],
         familyName: row[1],
